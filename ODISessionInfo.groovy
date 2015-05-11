a202bd73a21c087a8c560762082c39f59a9adb06
Ravi Sankar Krothapalli <rskrothapalli@gmail.com>
2015-05-11 14:00:06 -0600

    Version 2 with odiInputStream

--- ODISessionInfo.groovy
+++ ODISessionInfo.groovy
@@ -1,11 +1,39 @@
-//Created by ODI Studio
 
+//Created by Ravi Sankar Krothapalli
 import oracle.odi.domain.runtime.session.OdiSession;
 import oracle.odi.domain.runtime.session.finder.IOdiSessionFinder;
 
-curSession = ((IOdiSessionFinder)odiInstance.getTransactionalEntityManager().getFinder(OdiSession.class)).findBySessionId(21437500);
-println "Current Scenario : " + curSession.getSourceScenarioTag();
-curSessionStepLogs = curSession.getSessionStepLogs();
-def cloSessionTaskLogs = {taskLog -> println "\tTask Log :" + taskLog.getTaskName1() + " - " + taskLog.getTaskName1() + " - " + taskLog.getTaskName2()+ taskLog.getTaskName2()};
-def cloSessionStepLogs = {stepLog -> println "Step Log :" + stepLog.getName(); stepLog.getSessionTaskLogs().each({cloSessionTaskLogs.call(it)})};
-curSessionStepLogs.each({cStepLog -> cloSessionStepLogs(cStepLog)});
+//Read input with odiInputStream variable
+odiInputStream.withReader { reader ->
+  while (true) {
+    getSessionInformation(reader.readLine());
+  }
+}
+
+def getSessionInformation(sessionID){
+    
+    //get the session instance
+    curSession = ((IOdiSessionFinder)odiInstance.getTransactionalEntityManager().getFinder(OdiSession.class)).findBySessionId(sessionID.toInteger());
+    println "Current Scenario : " + curSession.getSourceScenarioTag();
+    curSessionStepLogs = curSession.getSessionStepLogs();
+    println "Default Values of the session variables";
+    
+    //get default varibles with values
+    curSession.getSessionVariables().each({println "\t" + it.getName() + " : " + it.getDefaultValue()}); 
+    println "\n";
+    
+    //get session step task log info
+    def cloSessionTaskLogs = {taskLog -> 
+        println "\tTask Log :" + taskLog.getTaskName1() + " - " + taskLog.getTaskName2() + " - " + taskLog.getTaskName3()+ taskLog.getTaskName2()
+        };
+        
+    //get session step log info     
+    def cloSessionStepLogs = {stepLog -> 
+        println "Step Log :" + stepLog.getName(); stepLog.getSessionTaskLogs().each({cloSessionTaskLogs.call(it)})
+        };
+        
+    //loop through session steps    
+    curSessionStepLogs.each({cStepLog -> 
+        cloSessionStepLogs(cStepLog)
+        });
+}
